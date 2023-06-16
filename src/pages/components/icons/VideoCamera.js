24a6073cef4ee473cef0e1d126caaaa367eb8671
Micaela Analia Jofre <mkjofre@gmail.com>
2023-06-16 00:04:40 -0300

    Created components icons

---
+++ src/pages/components/icons/VideoCamera.js
@@ -0,0 +1,19 @@
+function VideoCamera({width, heigth}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={heigth}
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        d="M17.5 6.9v6.2l-4-2.7v4.1h-11v-9h11v4.1z"
+      ></path>
+    </svg>
+  );
+}
+
+export default VideoCamera;
