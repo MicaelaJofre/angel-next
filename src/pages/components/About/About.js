06dd7e380506d447f2061d5525f69e4d71ad7b8c
Micaela Analia Jofre <mkjofre@gmail.com>
2023-06-16 22:11:57 -0300

    fixed class

--- src/pages/components/About/About.js
+++ src/pages/components/About/About.js
@@ -21,11 +21,11 @@ const About = () => {
         </div>
         <div className="flex flex-col items-center lg:items-start justify-between w-full lg:w-7/12 gap-7">
           <div className="leading-6 max-w-2xl lg:w-11/12">
-            <p><span class="font-bold text-gray3">Soy un artista creativo </span> con amplia experiencia en producción musical e ingeniería de sonido. He pasado por varias etapas en mi carrera, acumulando más de 10 años de experiencia en los medios de música comercial y audiovisuales. Actualmente, desempeño el rol de productor musical y editor en Anónimo Films, una productora de contenidos audiovisuales ubicada en el oeste de Argentina.</p>
+            <p><span className="font-bold text-gray3">Soy un artista creativo </span> con amplia experiencia en producción musical e ingeniería de sonido. He pasado por varias etapas en mi carrera, acumulando más de 10 años de experiencia en los medios de música comercial y audiovisuales. Actualmente, desempeño el rol de productor musical y editor en Anónimo Films, una productora de contenidos audiovisuales ubicada en el oeste de Argentina.</p>
             <p>Comencé mi formación en estudios de grabación y luego me especialicé en la técnica de sonido y producción musical en la academia Cetear. Para ampliar mis conocimientos, decidí tomar diversos cursos en la Universidad de Mendoza, lo que me permitió comprender claramente el idioma y los objetivos del medio. A partir de entonces, me he convertido en un autodidacta del medio audiovisual, explorando un amplio camino lleno de posibilidades gracias a mi conocimiento adquirido.</p>
           </div>
           <div className="w-full lg:w-11/12 flex justify-center">
-            <Image className="opacity-50 max-w-2xl w-full h-full max-h-[345px] object-cover " src={cartel} width={1000} height={500} alt="Angel Pelletan piloteando drone"/>
+            <Image className="opacity-50 max-w-2xl w-full h-full max-h-[345px] object-cover" src={cartel} width={1000} height={500} alt="Angel Pelletan piloteando drone"/>
           </div>
         </div>
       </article>
